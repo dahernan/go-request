4b098d0b6e6ca51343321d5825e84e8f015bd095
David Hernandez <dahernan@gmail.com>
2013-10-12 15:19:55 +1100

    move from gospec to ginkgo

--- src/github.com/dahernan/request/request_test.go
+++ src/github.com/dahernan/request/request_test.go
@@ -1,121 +1,137 @@
-package request
+package request_test
 
 import (
+	. "github.com/dahernan/request"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	"encoding/json"
 	"fmt"
 	simplejson "github.com/bitly/go-simplejson"
-	"github.com/orfjackal/gospec/src/gospec"
-	. "github.com/orfjackal/gospec/src/gospec"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
 )
 
-func RequestSpec(c gospec.Context) {
-	// setup http server for testing
+var _ = Describe("Request", func() {
+	Describe("Can make basic request sending and receiving JSON", func() {
+		It("can do a GET", func() {
+			ts := httptest.NewServer(http.HandlerFunc(jsonServer(jsonBuilder)))
+			url := ts.URL
+			defer ts.Close()
+			request := NewRequest(url)
 
-	c.Specify("GET request returns json object", func() {
-		ts := httptest.NewServer(http.HandlerFunc(JsonServer(JsonBuilder)))
-		url := ts.URL
-		defer ts.Close()
-		request := NewRequest(url)
+			body, _ := request.Get("/")
+			Expect(body.Get("message").MustString()).To(Equal("hello"))
+		})
 
-		body, _ := request.Get("/")
-		c.Expect(body.Get("message").MustString(), Equals, "hello")
+		It("can do a POST", func() {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				Expect(req.Method).To(Equal("POST"))
+				Expect(req.URL.String()).To(Equal("/hello/world"))
+				Expect(req.Header.Get("Content-Type")).To(Equal("application/json"))
+				Expect(req.Header.Get("Accept")).To(Equal("application/json"))
 
-	})
-	c.Specify("POST request can send and receive json", func() {
-		// server that received the request
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			c.Expect(req.Method, Equals, "POST")
-			c.Expect(req.URL.String(), Equals, "/hello/world")
-			c.Expect(req.Header.Get("Content-Type"), Equals, "application/json")
-			c.Expect(req.Header.Get("Accept"), Equals, "application/json")
+				body, error := ioutil.ReadAll(req.Body)
+				Expect(error).To(BeNil())
+				jsonObject, error := simplejson.NewJson(body)
+				Expect(error).To(BeNil())
+				Expect(jsonObject.Get("one").MustString()).To(Equal("1 one"))
+				Expect(jsonObject.Get("two").MustString()).To(Equal("2 two"))
+				sendOK(w)
+			}))
+			url := ts.URL
+			defer ts.Close()
+			request := NewRequest(url)
 
-			body, error := ioutil.ReadAll(req.Body)
-			assertNotError(error, c)
-			jsonObject, error := simplejson.NewJson(body)
-			assertNotError(error, c)
-			c.Expect(jsonObject.Get("one").MustString(), Equals, "1 one")
-			c.Expect(jsonObject.Get("two").MustString(), Equals, "2 two")
-			sendOK(w)
-		}))
-		url := ts.URL
-		defer ts.Close()
-		request := NewRequest(url)
+			jsonMap := make(map[string]string)
+			jsonMap["one"] = "1 one"
+			jsonMap["two"] = "2 two"
+			jsonBytes, _ := json.Marshal(jsonMap)
+			jsonObject, _ := simplejson.NewJson(jsonBytes)
 
-		jsonMap := make(map[string]string)
-		jsonMap["one"] = "1 one"
-		jsonMap["two"] = "2 two"
-		jsonBytes, _ := json.Marshal(jsonMap)
-		jsonObject, _ := simplejson.NewJson(jsonBytes)
+			// method to test
+			body, error := request.Post("/hello/world", jsonObject)
 
-		// method to test
-		body, error := request.Post("/hello/world", jsonObject)
+			Expect(error).To(BeNil())
+			ok, error := body.Get("ok").Bool()
+			Expect(error).To(BeNil())
+			Expect(ok).To(BeTrue())
+		})
 
-		assertNotError(error, c)
-		ok, error := body.Get("ok").Bool()
-		assertNotError(error, c)
-		c.Expect(ok, Equals, true)
+		It("Can do a PUT", func() {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				Expect(req.Method).To(Equal("PUT"))
+				sendOK(w)
+			}))
+			url := ts.URL
+			defer ts.Close()
+			request := NewRequest(url)
 
-	})
+			jsonMap := make(map[string]string)
+			jsonMap["one"] = "1 one"
+			jsonBytes, _ := json.Marshal(jsonMap)
+			jsonObject, _ := simplejson.NewJson(jsonBytes)
 
-	c.Specify("PUT request can send and receive json", func() {
-		// server that received the request
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			c.Expect(req.Method, Equals, "PUT")
-			sendOK(w)
-		}))
-		url := ts.URL
-		defer ts.Close()
-		request := NewRequest(url)
+			// method to test
+			body, error := request.Put("/hello/a/put", jsonObject)
 
-		jsonMap := make(map[string]string)
-		jsonMap["one"] = "1 one"
-		jsonBytes, _ := json.Marshal(jsonMap)
-		jsonObject, _ := simplejson.NewJson(jsonBytes)
+			Expect(error).To(BeNil())
+			ok, error := body.Get("ok").Bool()
+			Expect(error).To(BeNil())
+			Expect(ok).To(BeTrue())
 
-		// method to test
-		body, error := request.Put("/hello/a/put", jsonObject)
+		})
 
-		assertNotError(error, c)
-		ok, error := body.Get("ok").Bool()
-		assertNotError(error, c)
-		c.Expect(ok, Equals, true)
+		It("Can do a DELETE", func() {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				Expect(req.Method).To(Equal("DELETE"))
+				sendOK(w)
+			}))
+			url := ts.URL
+			defer ts.Close()
+			request := NewRequest(url)
 
-	})
+			jsonMap := make(map[string]string)
+			jsonMap["one"] = "1 one"
+			jsonBytes, _ := json.Marshal(jsonMap)
+			jsonObject, _ := simplejson.NewJson(jsonBytes)
 
-	c.Specify("DELETE request can send and receive json", func() {
-		// server that received the request
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			c.Expect(req.Method, Equals, "DELETE")
-			sendOK(w)
-		}))
-		url := ts.URL
-		defer ts.Close()
-		request := NewRequest(url)
+			// method to test
+			body, error := request.Delete("/hello/delete", jsonObject)
 
-		jsonMap := make(map[string]string)
-		jsonMap["one"] = "1 one"
-		jsonBytes, _ := json.Marshal(jsonMap)
-		jsonObject, _ := simplejson.NewJson(jsonBytes)
+			Expect(error).To(BeNil())
+			ok, error := body.Get("ok").Bool()
+			Expect(error).To(BeNil())
+			Expect(ok).To(BeTrue())
 
-		// method to test
-		body, error := request.Delete("/hello/delete", jsonObject)
-
-		assertNotError(error, c)
-		ok, error := body.Get("ok").Bool()
-		assertNotError(error, c)
-		c.Expect(ok, Equals, true)
+		})
 
 	})
-}
+
+	Describe("Handle errors", func() {
+		It("should hanlde a 404", func() {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				Expect(req.Method).To(Equal("GET"))
+				sendNotFound(w)
+			}))
+			url := ts.URL
+			defer ts.Close()
+			request := NewRequest(url)
+			body, error := request.Get("/")
+			fmt.Printf("Body: %s Error %s", body, error)
+			Expect(body.Get("exists").MustString()).To(Equal("false"))
+		})
+
+	})
+
+})
 
 type httpHandlerFunc func(w http.ResponseWriter, req *http.Request)
 type jsonHttpBuilderFunc func(req *http.Request) interface{}
 
-func JsonBuilder(req *http.Request) interface{} {
+func jsonBuilder(req *http.Request) interface{} {
 	jsonMap := make(map[string]interface{})
 	name := req.URL.Query().Get(":name")
 	jsonMap["message"] = "hello" + name
@@ -129,24 +145,26 @@ func sendOK(w http.ResponseWriter) {
 	writeJsonBytes(w, jsonBytes)
 }
 
+func sendNotFound(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+	jsonMap := make(map[string]interface{})
+	jsonMap["exists"] = false
+	jsonBytes, _ := json.Marshal(jsonMap)
+	writeJsonBytes(w, jsonBytes)
+
+}
+
 func writeJsonBytes(w http.ResponseWriter, jsonBytes []byte) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Content-Length", strconv.Itoa(len(jsonBytes)))
 	w.Write(jsonBytes)
 }
 
-func JsonServer(builderFunc jsonHttpBuilderFunc) (hanlderFunc httpHandlerFunc) {
+func jsonServer(builderFunc jsonHttpBuilderFunc) (hanlderFunc httpHandlerFunc) {
 	hanlderFunc = func(w http.ResponseWriter, req *http.Request) {
 		jsonObject := builderFunc(req)
 		jsonBytes, _ := json.Marshal(jsonObject)
 		writeJsonBytes(w, jsonBytes)
 	}
 	return
-}
-
-func assertNotError(err interface{}, c gospec.Context) {
-	if err != nil {
-		fmt.Printf("\tError: %s\n", err)
-	}
-	c.Expect(err, IsNil)
 }
